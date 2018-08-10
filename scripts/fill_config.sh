0d9d40aec4f6f0bdbc5f6f8032e4a3f7524a9986
pedromslopes <pms.lopes@campus.fct.unl.pt>
2018-08-10 17:44:04 +0100

    Changed fill_config script

--- scripts/fill_config.sh
+++ scripts/fill_config.sh
@@ -1,13 +1,48 @@
 #!/usr/bin/env bash
 
-echo "Writting ips to config files..."
-sed -i "s/Nodes/[\"$1\", \"$2\", \"$3\"]/g" ./config/**/*.config
-sed -i "s/AntidoteNames/[\"antidote@$1\", \"antidote@$2\", \"antidote@$3\"]/g" ./config/**/*.config
+function join_by { local IFS="$1"; shift; echo "$*"; }
+
+echo "$@"
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+	key="$1"
+
+	case $key in
+		  -i|--ip)
+		  IPS+=("$2")
+		  shift # past argument
+		  shift # past value
+		  ;;
+		  -p|--port)
+		  PORTS+=("$2")
+		  shift # past argument
+		  shift # past value
+		  ;;
+		  *)    # unknown option
+		  POSITIONAL+=("$1") # save it in an array for later
+		  shift # past argument
+		  ;;
+	esac
+done
+
+PREPARE_IPS=($(for ip in ${IPS[@]}; do echo "\"$ip\""; done))
+PREPARE_ANTIDOTENODES=($(for ip in ${IPS[@]}; do echo "\"antidote@$ip\""; done))
+
+JOIN_NODES=$(join_by "," ${PREPARE_IPS[@]})
+JOIN_ANTIDOTENODES=$(join_by "," ${PREPARE_ANTIDOTENODES[@]})
+
+
+echo "Writting ips to config files: ${JOIN_NODES[@]}"
+sed -i "s/Nodes/[${JOIN_NODES[@]}]/g" ./config/**/*.config
+sed -i "s/AntidoteNames/[${JOIN_ANTIDOTENODES[@]}]/g" ./config/**/*.config
 echo "Done"
 
-if [[ -n "$4" && -n "$5" && -n "$6" ]]; then
-	echo "Writting ports to config files..."
-	sed -i "s/Ports/[$4, $5, $6]/g" ./config/**/*.config
+if [ "${#PORTS[@]}" -gt 0 ]; then
+	JOIN_PORTS=$(join_by "," ${PORTS[@]})
+	echo "Writting ports to config files: ${JOIN_PORTS[@]}"
+	sed -i "s/Ports/[${JOIN_PORTS[@]}]/g" ./config/**/*.config
 	echo "Done"
 fi
 
@@ -19,5 +54,5 @@ sed -i "s/BASHOIP/$MY_IP/g" ./scripts/connect_dcs.escript
 echo "Done"
 
 echo "Writing ips to escript file"
-sed -i "s/Nodes/[\"$1\", \"$2\", \"$3\"]/g" ./scripts/connect_dcs.escript
+sed -i "s/Nodes/[${JOIN_NODES[@]}]/g" ./scripts/connect_dcs.escript
 echo "Done"
